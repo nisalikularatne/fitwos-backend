45ee53fae9c1481d6689dc0a2cb375fd86c72aa3
nisali kularatne <nisalikularatne@MacBook-Pro.local>
2021-07-28 22:46:25 +0800

    initial knex boilerplate

---
+++ knexfile.js
@@ -0,0 +1,53 @@
+require("dotenv").config();
+const pg = require("pg");
+// pg.defaults.ssl = true;
+console.log('check pool size',process.env.NODE_ENV);
+module.exports = {
+  development: {
+    client: "pg",
+    useNullAsDefault: true,
+    connection: process.env.DEV_DATABASE_URL,
+    pool: {
+      min: Number(process.env.DEV_DB_MIN_POOL) || 0,
+      max: Number(process.env.DEV_DB_MAX_POOL) || 1
+    },
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+
+  testing: {
+    client: "pg",
+    connection: {
+      host: process.env.POSTGRES_TEST_HOST,
+      port: process.env.POSTGRES_TEST_PORT,
+      user: process.env.POSTGRES_TEST_USER,
+      password: process.env.POSTGRES_TEST_PASSWORD,
+      database: process.env.POSTGRES_TEST_DATABASE
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  },
+
+  production: {
+    client: "pg",
+    useNullAsDefault: true,
+
+    connection: process.env.PROD_DATABASE_URL,
+
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
+  }
+};
