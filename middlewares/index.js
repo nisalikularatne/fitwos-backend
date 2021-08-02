54bd2acdf7734f50fed2537c4a74f202c2c29271
nisali kularatne <nisalikularatne@MacBook-Pro.local>
2021-08-02 23:45:07 +0800

    token validation

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,17 +1,31 @@
-const { FitwosError } = require('../utils/errors');
+const {FitwosError} = require('../utils/errors');
+require('dotenv').config({ path: '../../.env' });
+const jwt = require('jsonwebtoken');
 exports.authentication = async (req, res, next) => {
     let userInfo = req.headers['x-userinfo'];
+    let authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) {
+        let error = new FitwosError('User token is missing', 1001, 401);
+        res.status(400).send(error);
+    }
     if (!userInfo) {
         let error = new FitwosError('User info header missing', 1000, 401);
         res.status(400).send(error);
     }
-    const buff = Buffer.from(userInfo, 'base64');
-    const user = buff.toString('utf-8');
-
+    let token = authorizationHeader.substr(7, authorizationHeader.length - 1);
+    if (token == null) return res.sendStatus(401);
+    await jwt.verify(token,process.env.PUBLIC_KEY.replace(/\\n/g, '\n'),{ algorithms: ['RS256']}, (err, user_info) => {
+        // eslint-disable-next-line no-console
+        console.log(err);
+        if (err) return res.sendStatus(403);
+        const buff = Buffer.from(userInfo, 'base64');
+        const user = buff.toString('utf-8');
 // print normal string
-    req.user = user
-    console.log('show the user',user);
-    next();
+        req.user = user
+        console.log('show the user', user);
+        next();
+    });
+
 
 }
 ;
