29a0bb8ebaaef3b5b3e95122c21ce3838f39b9c0
Nisali Kularatne <nisali@socar.my>
2021-10-19 15:44:00 +0800

    user get added according to feedback to include image

--- services/users/index.js
+++ services/users/index.js
@@ -4,21 +4,22 @@ const User= require('../../models/user')
 const axios = require('axios');
 // const config = require('@fitwos/fitwos-application/config');
 exports.getUser = async({ headers }) => {
+    let user;
    let userInfo = await axios.get('https://kong.fitwos.club/auth/realms/fitwos/protocol/openid-connect/userinfo', {
        headers: {
            Authorization: headers.authorization //the token is a variable which holds the token
        }
    })
     if(userInfo.data){
-        const user = await User.query().where('user_uuid', userInfo.data.sub).first();
+        user = await User.query().where('user_uuid', userInfo.data.sub).first();
         if(!user){
             const {given_name,family_name,sub,email,email_verified,name} = userInfo.data;
-            await User.query().insert({
+            user = await User.query().insert({
                 given_name,family_name,user_uuid:sub,email,email_verified,name
             })
         }
     }
-    return userInfo.data
+    return user;
 };
 exports.saveImage = async (image_url,id)=>{
     const updatedObject = await User.query().findOne({user_uuid: id}).throwIfNotFound();
